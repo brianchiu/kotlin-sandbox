dae1fb1ea502e12be468c9224c0ab47d0ac5038a
Brian Chiu <brian.chiu@liferay.com>
2017-09-01 16:07:05 -0700

    Conform poshi page object and steps to the rest of the repo

--- src/test/kotlin/com/liferay/sandbox/QAChallengePoshiPageObject.kt
+++ src/test/kotlin/com/liferay/sandbox/QAChallengePoshiPageObject.kt
@@ -2,15 +2,12 @@ package com.liferay.sandbox;
 
 import com.liferay.poshi.runner.selenium.SeleniumUtil
 
-import cucumber.api.java.en.When
-
 import java.lang.String
 
-class Poshi {
+class QAChallengePoshiPageObject {
     val selenium = SeleniumUtil.getSelenium()
 
-    @When(value = "^I use poshi$")
-    fun poshiTest() {
+    fun doQAChallengeStepsWithPoshi() {
         selenium.open("http://www.liferay.com")
         selenium.click("//a[@id='menuToggle']")
         selenium.waitForVisible("//input[@class='doc-search-input']")
