cfd9ca629e49a9597b4abb1b3db7bbf1f0a40f46
Brian Chiu <brian.chiu@liferay.com>
2017-09-08 15:14:22 -0700

    Start fresh

--- src/test/kotlin/com/liferay/sandbox/BasePageObject.kt
+++ src/test/kotlin/com/liferay/sandbox/BasePageObject.kt
@@ -1,83 +1,15 @@
-package com.liferay.sandbox
+package com.liferay.sandbox;
 
-import org.junit.Assert
-import org.openqa.selenium.By
-import org.openqa.selenium.WebElement
-import java.util.concurrent.TimeUnit
+import com.liferay.poshi.runner.selenium.SeleniumUtil
 
-open class BasePageObject {
-    private val webDriver = FirefoxInstance.hashi()
+class QAChallengePoshiPageObject {
+    private val selenium = SeleniumUtil.getSelenium()
 
-    fun assertText(locator : String, value : String) {
-        val webElement = getWebElement(locator)
-        val actualText = webElement.text
-
-        Assert.assertEquals(value, actualText)
-    }
-
-    fun assertVisible(locator : String) {
-        val webElement = getWebElement(locator)
-        Assert.assertEquals(true, webElement.isDisplayed)
-    }
-
-    fun click(locator : String) {
-        val webElement = getWebElement(locator)
-        webElement.click()
-    }
-
-    fun clickButton(button : String) {
-        val locator = "//button[contains(.,'$button')]"
-
-        click(locator)
-    }
-
-    fun clickLink(link: String) {
-        val locator = "//a[contains(.,'$link')]"
-
-        click(locator)
-    }
-
-    fun getWebElement(locator : String) : WebElement {
-        val webElements : List<WebElement> = getWebElements(locator)
-        if (webElements.isEmpty()) {
-            println("Unable to find : \"$locator\"")
-        }
-        return webElements[0]
-    }
-
-    fun getWebElements(locator : String) : List<WebElement> {
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
-        return webDriver.findElements(By.xpath(locator))
-    }
-
-    fun isVisible(locator : String) : Boolean {
-        val webElements : List<WebElement> = getWebElements(locator)
-        if (webElements.isEmpty()) {
-            return false
-        }
-        else {
-            return webElements[0].isDisplayed
-        }
-    }
-
-    fun open(url : String) {
-        webDriver.get(url)
-    }
-
-    fun quit() {
-        webDriver.quit()
-    }
-
-    fun type(locator : String, value : String) {
-        val webElement = getWebElement(locator)
-        webElement.clear();
-        webElement.sendKeys(value)
-    }
-
-    fun typeInput(field: String, text: String) {
-        val locator = "//div[label[contains(.,'$field')]]//input"
-
-        type(locator, text)
+    fun doQAChallengeStepsWithPoshi() {
+        selenium.open("http://www.liferay.com")
+        selenium.click("//a[@id='menuToggle']")
+        selenium.waitForVisible("//input[@class='doc-search-input']")
+        selenium.type("//input[@class='doc-search-input']", "Liferay DXP Tech Specs")
+        selenium.click("//a[@class='submit-search']")
     }
 }
-
--- src/test/resources/features/QAChallenge.feature
+++
@@ -1,7 +0,0 @@
-Feature: QA Challenge
-
-Scenario: Try out with Poshi
-	When I do the QA challenge using Poshi
-
-Scenario: Try out with WebDriver
-	When I do the QA challenge using WebDriver
