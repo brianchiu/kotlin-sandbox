e476de2bda8cd083b1db37dbfa376cd3c31c31e8
Brian Chiu <brian.chiu@liferay.com>
2017-09-14 17:07:45 -0700

    SF

--- src/test/kotlin/com/liferay/steps/portalsmoke/PortalSmokeSteps.kt
+++ src/test/kotlin/com/liferay/steps/portalsmoke/PortalSmokeSteps.kt
@@ -10,13 +10,10 @@ import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
 
 class BaseSteps {
-    private val _addPanelPO = AddPanelPO()
-    private val _loginPO = LoginPO()
-    private val _productMenuPO = ProductMenuPO()
-    private val _webContentPO = WebContentPO()
-
     @Then(value = "^I should be able to add apps$")
     fun addApps() {
+        val _addPanelPO = AddPanelPO()
+
         _addPanelPO.addApp("Blogs")
         _addPanelPO.addApp("Web Content Display")
         _addPanelPO.addApp("Documents and Media")
@@ -24,12 +21,17 @@ class BaseSteps {
 
     @When(value = "^I add web content$")
     fun addContent() {
+        val _productMenuPO = ProductMenuPO()
+        val _webContentPO = WebContentPO()
+
         _productMenuPO.goToAppAdmin("Content", "Web Content")
         _webContentPO.add("Web Content Title", "Web Content Content")
     }
 
     @Then(value = "^I should be able to add pages$")
     fun addPages() {
+        val _productMenuPO = ProductMenuPO()
+
         _productMenuPO.addPage("Test Page 1")
         _productMenuPO.addPage("Test Page 2")
         _productMenuPO.addPage("Test Page 3")
@@ -37,11 +39,15 @@ class BaseSteps {
 
     @Then(value = "^I should be able to see the web content$")
     fun viewContent() {
+        val _webContentPO = WebContentPO()
+
         _webContentPO.view("Web Content Title", "Web Content Content")
     }
 
     @Given(value = "^I am logged in as the admin user$")
     fun login() {
+        val _loginPO = LoginPO()
+
         _loginPO.login("test@liferay.com", "test")
     }
 }
